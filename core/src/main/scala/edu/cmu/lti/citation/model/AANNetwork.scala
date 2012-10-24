64773470815fea0da3e23e3ce59b1ba71e870bc8
hunterhector <hunterhector@gmail.com>
2012-10-24 15:00:48 -0400

    excluding local project files from github

---
+++ core/src/main/scala/edu/cmu/lti/citation/model/AANNetwork.scala
@@ -0,0 +1,12 @@
+package edu.cmu.lti.citation.model
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: Hector, Zhengzhong Liu
+ * Date: 10/23/12
+ * Time: 4:36 PM
+ * To change this template use File | Settings | File Templates.
+ */
+class AANNetwork {
+
+}
